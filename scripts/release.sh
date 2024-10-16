36d916362a75cf7e121a68e639591c468fed7a53
Felipe Santos <felipecassiors@gmail.com>
2024-10-16 20:28:15 -0300

    Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+version="${1}"
+
+echo "Releasing version ${version}..."
+
+script_path=$(realpath "$0")
+script_dir=$(dirname "${script_path}")
+build_dir="$(realpath "${script_dir}/..")"
+readonly build_dir
+unset script_path script_dir
+
+docker build "${build_dir}" --pull --push \
+  --tag "ghcr.io/felipecrs/dond-shim-bin:${version}"
+docker build "${build_dir}" --pull --push \
+  --target dond-shim-bin --tag "ghcr.io/felipecrs/dond-shim-bin:${version}"
+
+git_sha=$(git rev-parse HEAD)
+gh release create "v${version}" --title "v${version}" --target "${git_sha}"
