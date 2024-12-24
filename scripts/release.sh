d3f4b047138b0536b7ba41caa6a8eeaad3cd5735
Felipe Santos <felipecassiors@gmail.com>
2024-12-24 20:12:17 -0300

    Fix docker build and push in release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,9 +13,11 @@ readonly build_dir
 unset script_path script_dir
 
 docker build "${build_dir}" --pull --push \
-  --tag "ghcr.io/felipecrs/dond-shim-bin:${version}"
+  --tag "ghcr.io/felipecrs/dond-shim:${version}" \
+  --tag "ghcr.io/felipecrs/dond-shim:latest"
 docker build "${build_dir}" --pull --push \
-  --target dond-shim-bin --tag "ghcr.io/felipecrs/dond-shim-bin:${version}"
+  --target dond-shim-bin --tag "ghcr.io/felipecrs/dond-shim-bin:${version}" \
+  --tag "ghcr.io/felipecrs/dond-shim-bin:latest"
 
 git_sha=$(git rev-parse HEAD)
 gh release create "v${version}" --title "v${version}" --target "${git_sha}"
